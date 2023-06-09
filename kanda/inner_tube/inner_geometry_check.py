3c1999c51f83188128341a12b130bfb31e4b5d8a
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-06-09 17:48:28 +0900

    inner ver6作成

--- kanda/inner_tube/inner_geometry_check.py
+++ kanda/inner_tube/inner_geometry_check.py
@@ -5,32 +5,42 @@ from textwrap import fill
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import tight_layout
 
 version = 'ver2'
 
-fig = plt.figure(figsize=(10, 7))
+fig = plt.figure(facecolor='gray',figsize=(16, 7), tight_layout=True)
 ax =plt.axes()
 
 
-domain = patches.Rectangle(xy=(0, 0), width=92, height=77, fc='c', label='PML')
-basalt_box = patches.Rectangle(xy=(1, 1), width=90, height=75, fc='k', label='Basalt')
-tube_box = patches.Rectangle(xy=(31, 31), width=30, height=15, fc='w', label='Tube')
+domain = patches.Rectangle(xy=(0, 0), width=132, height=77, fc='c', label='PML')
+basalt_box_1 = patches.Rectangle(xy=(1, 1), width=130, height=30, fc='k', label='Basalt')
+basalt_box_2 = patches.Rectangle(xy=(1, 46), width=130, height=30, fc='k')
+
+tube_box = patches.Rectangle(xy=(1, 31), width=130, height=15, fc='w', label='Vacuum')
+tube_dent = patches.Rectangle(xy=(71, 26), width=5, height=5, fc='w')
+
 src_horizontal = patches.Arrow(x=38.5, y=32, dx=15, dy=0, width=1, fc='r', label='Source horizontal')
 src_vertical = patches.Arrow(x=46, y=41, dx=0, dy=10, width=1, fc='b', label='Source vertical') 
 
-src_x = 38.5
-src_y = 32
+src_x = 66
+src_y = 38.5
 src_point = patches.Circle(xy=(src_x, src_y), radius=0.5, fc='g', label='Source point')
 
 
 ax.add_patch(domain) 
-ax.add_patch(basalt_box)
+ax.add_patch(basalt_box_1)
+ax.add_patch(basalt_box_2)
+
 ax.add_patch(tube_box)
-ax.add_patch(src_horizontal)
+ax.add_patch(tube_dent)
+
+#ax.add_patch(src_horizontal)
 #ax.add_patch(src_vertical)
 ax.add_patch(src_point)
 
-time = [0.58, 1.03, 1.09, 1.21, 1.9, 2.33, 5.13, 6]
+
+time = [0.592, 1.09, 1.59]
 tx_time = np.ones_like(time) * 0.088
 print(tx_time)
 
@@ -42,7 +52,7 @@ for i in range(len(tau)):
     ax.add_patch(range_i)
 
 # set legend at the outside of the plot
-ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=12)
+ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=20)
 
 #plt.title(version, fontsize=20)
 ax.set_xlabel('x [m]', fontsize=16)
