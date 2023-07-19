bc0f7f4d9f6ac713807fc25094ba2d877c97150d
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-07-19 10:14:50 +0900

    np.mean追加

--- kanda/domain_20x10/4region/B-scan/identigy_permittivity.py
+++ kanda/domain_20x10/4region/B-scan/identigy_permittivity.py
@@ -43,6 +43,8 @@ for i in range(len(time_array)):
     v_ground = 2 * L /np.sqrt(time_oblique_ground**2 - time_perp**2) - c * time_array[i] / time_oblique_ground
     epsilon_r2[i] = (c / v_ground)**2
 
+mean = np.mean(epsilon_r2)
+print(mean)
 
 plt.plot(index, epsilon_r1, label='no vacuum rivised')
 plt.plot(index, epsilon_r2, label='vacuum rivised')
