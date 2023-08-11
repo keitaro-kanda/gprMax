bc58baebc381504f2e229df78c61147fd3f44661
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-08-11 18:10:10 +0900

    delete stack_png.py

--- tools/stack_png.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-# pngファイルの読み込み
-png = np.loadtxt('kanda/domain_10x10/test/B-scan/smooth_2_biarray/migration_results/migration_result1.png', delimiter=',')
-print(png.shape)
-print(png)
