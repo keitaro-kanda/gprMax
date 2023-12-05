74ecd6d7cad934832929f740bf5a8388dc40a24d
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2023-12-05 18:37:32 +0900

    .astypeを使用してインデックス番号の計算を事前に済ませる

--- tools/Su_method.py
+++ tools/Su_method.py
@@ -2,16 +2,11 @@ import argparse
 import itertools
 import json
 import os
-from cProfile import label
-from math import tau
-from re import A
-from socket import AI_V4MAPPED_CFG
 
 import h5py
 import matplotlib.pyplot as plt
 import mpl_toolkits.axes_grid1 as axgrid1
 import numpy as np
-from requests import get
 from tqdm import tqdm
 
 from tools.outputfiles_merge import get_output_data
@@ -50,6 +45,11 @@ ground_depth = params['ground_depth']
 antenna_height = params['antenna_height']
 domain_z = ground_depth + antenna_height # アンテナ高さをz=0とする
 domain_array = np.zeros((domain_z, domain_x))
+
+class get_data:
+    def __init__(self):
+        self.rx_posi = int()
+
 
 rx1, dt = get_output_data(data_path, 1, 'Ez') # shape=(29679, 10)
 rx2, dt = get_output_data(data_path, 2, 'Ez')
@@ -98,17 +98,20 @@ tau_src2ref = L_src2ref / (c / np.sqrt(4)) # tau: [s], 試しに誘電率4で計
 # calculate cross-correlation
 cross_corr = np.zeros((domain_z, domain_x))
 
-def calc_Amp(i): # i: 0~antenna_num-1を入力する
+def calc_Amp(z, x, i): # i: 0~antenna_num-1を入力する
     # i番目のrxにおける遅れ時間配列（1D）を作成
     # 簡単のため，i番目のsrcで送信してi番目のrxで受診するのもOKとする
-    tau = 5e-9 + tau_src2ref[z, x, :] + tau_ref2rx[z, x, i] # i番目のrxで受信する場合の遅れ時間[s]
+    tau = 4e-9 + tau_src2ref[z, x, :] + tau_ref2rx[z, x, i] # i番目のrxで受信する場合の遅れ時間[s]
+    tau_index = (tau / dt).astype(int) # tauをインデックス番号に変換
 
     # i番目のrxにおける振幅配列（1D）を作成
     Amp_array = np.zeros(antenna_num)# １次元配列, 行：rxインデックス, 列：srcインデックス
+
+    
     for src in range(antenna_num):
         # i番目のrxにおける，src番目のsrcで送信したときの振幅を取得
         rx_data, dt = get_output_data(data_path, i+1, 'Ez')
-        Amp_array[src] = rx_data[int(tau[src]/dt), src]
+        Amp_array[src] = rx_data[tau_index, src]
     return Amp_array
 
 def calc_corr():
@@ -119,7 +122,7 @@ def calc_corr():
         for z in tqdm(range(domain_z), desc='x='+str(x)):
             corr_list = [] # ここで毎回リストを初期化する
             for rx in range(antenna_num):
-                Amp_at_xz[rx] = calc_Amp(rx)
+                Amp_at_xz[rx] = calc_Amp(z, x, rx)
             # パスの組み合わせを取り出して積を計算
             for pair in itertools.permutations(Amp_at_xz[:, :], 2):
                 corr_list.append(np.abs(pair[0]) * np.abs(pair[1]))
@@ -132,6 +135,7 @@ def calc_corr():
 corr = calc_corr()
 np.savetxt(data_dir_path + '/corr.csv', corr, delimiter=',')
 
+""""
 def calc_corr_rx1():
     # forループの準備
     corr_list = []
@@ -140,6 +144,7 @@ def calc_corr_rx1():
     for x in tqdm(range(domain_x)):
         for z in range(domain_z):
             tau = 5e-9 + tau_src2ref[z, x, 1:] + tau_ref2rx[z, x, 0] # tx1で送信の場合の遅れ時間[s]
+            tau_index = (tau / dt).astype(int) # tauをインデックス番号に変換
             #  tauに対応するrx1の振幅を取得
             Amp =  np.zeros(antenna_num-1)
             Amp[0] = rx1[int(tau[0]/dt), 1] # rx1, src2
@@ -163,6 +168,7 @@ def calc_corr_rx1():
     return cross_corr
 
 #corr_rx1 = calc_corr_rx1()
+"""
 
 # plot
 fig = plt.figure(facecolor='w', edgecolor='w')
