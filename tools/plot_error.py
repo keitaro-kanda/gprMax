373e8d980c012fd18183436c818b89a59904d986
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2024-02-12 17:38:26 +0900

    画像とCSVファイルの変更をコミットしました

--- tools/plot_error.py
+++ tools/plot_error.py
@@ -34,6 +34,7 @@ Vn_true = c / np.sqrt(epsilon_true) # [m/s], propagation velocity in each layers
 t0_estimated = np.array(params['tau_ver']) * 10**(-9) # [s]
 Vrms_estimated = np.array(params['V_RMS']) * c # [m/s]
 depth_estimated = t0_estimated * Vrms_estimated / 2 - params['antenna_height'] # [m], depth of each layers`
+thickness_estimated = np.diff(np.insert(depth_estimated, 0, 0)) # [m], thickness of each layers
 
 # load subsurface_structure.txt
 subsurface_structure = np.loadtxt(params['subsurface_structure'], delimiter=',')
@@ -45,18 +46,20 @@ Vn_estimated = np.nan_to_num(subsurface_structure[:,1] * c) # [m/s]
 t0_error = np.abs(t0_true - t0_estimated) / t0_true * 100
 Vrms_error = np.abs(Vrms_true - Vrms_estimated) / Vrms_true * 100
 depth_error = np.abs(depth_true - depth_estimated) / depth_true * 100
+thickness_error = np.abs(thickness_true - thickness_estimated) / thickness_true * 100
 epsilon_error = np.abs(epsilon_true - epsilon_estimated) / epsilon_true * 100
 Vn_error = np.abs(Vn_true - Vn_estimated) / Vn_true * 100
 
 
 
 #* plot error
-fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-ax.plot(depth_true, t0_error, label=r'$t_0$')
-ax.plot(depth_true, Vrms_error, label=r'$V_{rms}$')
-ax.plot(depth_true, depth_error, label='depth')
-ax.plot(depth_true, epsilon_error, label=r'$\varepsilon_r$')
-ax.plot(depth_true, Vn_error, label=r'$V_n$')
+fig, ax = plt.subplots(1, 1, figsize=(15, 15), tight_layout=True)
+ax.plot(depth_true, t0_error, marker='.', markersize=10, label=r'$t_0$')
+ax.plot(depth_true, Vrms_error, marker='.', markersize=10, label=r'$V_{rms}$')
+ax.plot(depth_true, depth_error, marker='.', markersize=10, label='depth')
+ax.plot(depth_true, thickness_error, marker='.', markersize=10, label='thickness')
+ax.plot(depth_true, Vn_error, marker='.', markersize=10, label=r'$V_n$')
+ax.plot(depth_true, epsilon_error, marker='.', markersize=10, label=r'$\varepsilon_r$')
 
 ax.set_xlabel('Depth of subsurface interface [m]', fontsize=20)
 ax.set_ylabel('Error to model value [%]', fontsize=20)
@@ -75,7 +78,14 @@ plt.show()
 
 
 #* save error values as csv
-error_values = np.array([t0_error, Vrms_error, depth_error, epsilon_error, Vn_error]).T
-header = 't0 [%], Vrms [%], depth [%], epsilon [%], Vn [%]'
+# estimated value
+estimated_values = np.array([t0_estimated, Vrms_estimated, depth_estimated, thickness_estimated,
+                            epsilon_estimated, Vn_estimated]).T
+header = 't0 [s], Vrms [m/s], depth [m], thickness [m], epsilon_r, Vn [m/s]'
+np.savetxt(os.path.join(json_dir, 'estimated.csv'), estimated_values, delimiter=',', header=header, comments='')
+# error
+error_values = np.array([t0_error, Vrms_error, depth_error, thickness_error,
+                        epsilon_error, Vn_error]).T
+header = 't0 [%], Vrms [%], depth [%], thickness [%], epsilon [%], Vn [%]'
 np.savetxt(os.path.join(json_dir, 'error.csv'), error_values, delimiter=',', header=header, comments='')
 
