415511e8ddf7743c44eeb65a760a0d527d476ea8
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2024-03-05 16:53:22 +0900

    ツール整理 De Pue eq11の修正

--- tools/estimate_Vrms_DePue.py
+++ tools/estimate_Vrms_DePue.py
@@ -9,20 +9,15 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.axes_grid1 as axgrid1
 from tqdm import tqdm
 
-from tools.outputfiles_merge import get_output_data
 import matplotlib as mpl
-from scipy.optimize import fsolve
 from multiprocessing import Pool
-from multiprocessing import freeze_support
-
 
 
 #* Parse command line arguments
-parser = argparse.ArgumentParser(description='Processing estimate Vrms in De Pue et al. method',
-                                usage='cd gprMax; python -m tools.estimate_Vrms_DePue jsonfile plot_type')
+parser = argparse.ArgumentParser(description='Processing De Pue et al. method',
+                                usage='cd gprMax; python -m tools.Su_method jsonfile plot_type -closeup')
 parser.add_argument('jsonfile', help='json file path')
-parser.add_argument('plot_type', choices=['plot', 'mask', 'select', 'calc'])
-parser.add_argument('-CMP', action='store_true', help='option: analyse as CMP observation')
+parser.add_argument('plot_type', choices=['plot', 'select', 'calc'])
 args = parser.parse_args()
 
 
@@ -32,22 +27,17 @@ with open (args.jsonfile) as f:
 
 
 
-#* Open output file and read number of outputs (receivers)
+#* check dt
 data_path = params['out_file']
 data = h5py.File(data_path, 'r')
-nrx = data.attrs['nrx']
+dt = data.attrs['dt']
 data.close()
-data_dir_path = os.path.dirname(data_path)
 
 
-
-#* load data
-data_list = []
-#if args.select_points == False:
-for i in range(1, nrx+1):
-    data, dt = get_output_data(data_path, i, 'Ez')
-    data_list.append(data)
-
+#* load CMP txt data
+data_path = params['cmp_file']
+data = np.loadtxt(data_path, delimiter=',')
+obs_num = data.shape[1]
 
 
 #* set physical constants
@@ -57,17 +47,15 @@ epsilon_0 = 1 # vacuum permittivity
 
 
 #* set calculation parameters
-RMS_velocity = np.arange(0.01, 1.01, 0.01) # percentage to speed of light, 0% to 100%
-vertical_delay_time = np.arange(0, params['time_window'], params['time_step']) # 2-way travel time in vertical direction, don't contans air travel time[ns]
+v_interval = 0.01 # [/c]
+RMS_velocity = np.arange(0.01, 1.01, v_interval) # percentage to speed of light, 0% to 100%
+vertical_delay_time = np.arange(0, params['time_window'], params['time_step']) # 2-way travelt time in vertical direction, [ns]
 
 
 
 #* load parameters from json file
 antenna_step = params['src_step'] # antenna distance step, [m]
-rx_start = params['rx_start'] # rx start position, [m]
-tx_start = params['src_start'] # src start position, [m]
 src_end = params['src_end'] # src end position, [m]
-src_move_times = nrx # the number of src move times
 pulse_width = int(params['pulse_length'] / dt) # [data point]
 transmit_delay = params['transmitting_delay'] # [ns]
 transmit_delay_point = int(transmit_delay / dt) # [data point]
@@ -79,154 +67,123 @@ antenna_height = params['antenna_height'] # [m]
 def DePue_eq9(y1, y0, z0, z1, v1):
     # De Pue eq(9b)
     lateral_air = (y0 - y1) / 2
-    sin0 = lateral_air / (lateral_air**2 + z0**2)
+    sin0 = lateral_air / np.sqrt(lateral_air**2 + z0**2)
     # De Pue eq(9c)
     lateral_ground = (y1 / 2)
-    sin1 = lateral_ground / (lateral_ground**2 + z1**2)
+    sin1 = lateral_ground / np.sqrt(lateral_ground**2 + z1**2)
 
-    return sin0 / sin1 - c / v1
+    return np.abs(sin0 / sin1 - c / v1)
 
 
 
 #* make function to calcurate semblance
-def calc_semblance(Vrms_ind, t0_ind, i): # i: in range(nrx)
+def calc_semblance(Vrms_ind, t0_ind, i): # i: in range(obs_num)
     Vrms = RMS_velocity[Vrms_ind] * c # [m/s]
     t0 = vertical_delay_time[t0_ind] * 10**(-9) # [s]
     depth = Vrms * t0 / 2 # [m]
-    semblance_array = []
 
-    #* CMP observation
-    if args.CMP:
-        rx_ind = i
-        tx_ind = nrx - 1 - i
+    # calculate ground offset
+    #offset = antenna_step * 2 * (i + 1) # [m]
+    rx_posi = antenna_step * i # [m]
+    tx_posi = src_end - antenna_step * i # [m]
+    offset = np.abs(rx_posi - tx_posi) # [m]
+    if offset == 0:
+        offset_ground_solution = 0
+    else:
+        offset_ground = np.linspace(offset/100, offset, 100)
+        DePue_eq9_result = DePue_eq9(offset_ground, offset, antenna_height, depth, Vrms)
+        offset_ground_solution = offset_ground[np.argmin(DePue_eq9_result)]
+        #if np.min(DePue_eq9_result) > 0.5 and np.min(DePue_eq9_result) < 1.0:
+            #print('Error: DePue_eq9_result is too large at Vrms: {}, t0: {}, offset: {}, \
+            #    result is {}'.format(Vrms_ind, t0_ind, offset, np.min(DePue_eq9_result)))
 
-        # calculate offset
-        rx_posi = rx_start + rx_ind * antenna_step # [m]
-        tx_posi = tx_start + tx_ind * antenna_step # [m]
-        offset = np.abs(rx_posi - tx_posi) # [m]
-
-
-        # calculate offset_ground
-        if offset == 0:
-            offset_ground_solution = 0
-        else:
-            offset_ground = np.linspace(offset/100, offset, 100)
-            DePue_eq9_result = DePue_eq9(offset_ground, offset, antenna_height, depth, Vrms)
-            offset_ground_solution = offset_ground[np.argmin(np.abs(DePue_eq9_result))]
-
-
-        # calculate delay time
-        delay_time = np.sqrt((offset - offset_ground_solution)**2 + 4 * antenna_height**2) / c \
-            + t0  + offset_ground_solution**2 / Vrms**2
+    # calculate delay time
+    delay_time = np.sqrt((offset - offset_ground_solution)**2 + 4 * antenna_height**2) / c \
+            + np.sqrt(offset_ground_solution**2 + 4 * depth**2) / Vrms # [s]
+    
+    # check if surface wave or not
+    delay_surface = np.sqrt(offset**2 + 4 * antenna_height**2) / c
+    if delay_time < delay_surface:
+        total_delay_point = 0
+    else:
         total_delay_time = delay_time + transmit_delay * 10**(-9) # [s]
         total_delay_point = int(total_delay_time / dt) # data point
 
 
-        # search data
-        if total_delay_point >= len(data_list[i]):
-            semblance_array.append(0)
-        else:
-            semblance_array.append(np.abs(data_list[rx_ind][total_delay_point, tx_ind]))
-
-
-    #* array observation
+    # search data
+    if total_delay_point >= data.shape[0]:
+        echo_intensity = 0
     else:
-        rx_posi = rx_start + i * antenna_step # [m]
-        for src in range(src_move_times):
-            src_posi = tx_start + src * antenna_step # [m]
-            offset = np.abs(src_posi - rx_posi) # [m]
+        echo_intensity = np.abs(data[total_delay_point, i])
 
-            # calculate offset_ground
-            if offset == 0:
-                offset_ground_solution = 0
-                continue
-            else:
-                offset_ground = np.linspace(offset/100, offset, 100) # [m]
-                #print('y1: {}, y0: {}, z0: {}, z1: {}'.format(offset_ground, offset, antenna_height, depth))
-                #offset_ground_solution = fsolve(DePue_eq9, 0.1, args=(offset, antenna_height, depth, Vrms), maxfev=2000)
-                DePue_eq9_result = DePue_eq9(offset_ground, offset, antenna_height, depth, Vrms)
-                offset_ground_solution = offset_ground[np.argmin(np.abs(DePue_eq9_result))]
-
-
-            # calculate delay time
-            delay_time = np.sqrt((offset - offset_ground_solution)**2 + 4 * antenna_height**2) / c \
-                + t0  + offset_ground_solution**2 / Vrms**2 # [s]
-            total_delay_time = delay_time + transmit_delay * 10**(-9) # [s]
-            total_delay_point = int(total_delay_time / dt) # data point
-
-
-            # search data
-            if total_delay_point >= len(data_list[i]):
-                semblance_array.append(0)
-                continue
-            else:
-                semblance_array.append(np.abs(data_list[i][total_delay_point, src]))
-
-    return semblance_array
+    return echo_intensity
 
 
 
-def calc_semblance_helper(args):
-    v, t, i = args
-    return calc_semblance(v, t, i)
-
-
-
+"""
+#! Currently not used
 #* roop calc_semblance
 def roop():
     semblance_map = np.zeros((len(vertical_delay_time), len(RMS_velocity)))
-    """
-    # Use multiprocessing to parallelize the outer loop
-    with Pool() as pool:
-        results = list(tqdm(pool.imap(calc_semblance_helper,
-                        [(v, t, i) for v in range(len(RMS_velocity)) for t in range(len(vertical_delay_time)) for i in range(nrx)]),
-                        total=len(RMS_velocity)*len(vertical_delay_time), desc='calc_semblance'))
 
-
-    # Reshape the results back into semblance_map
-    for v in range(len(RMS_velocity)):
-        for t in range(len(vertical_delay_time)):
-            index = v * len(vertical_delay_time) * nrx + t * nrx
-            semblance_map[t, v] = np.array(results[index:index + nrx]).sum() ** 2
-    """
     for v in tqdm(range(len(RMS_velocity)), desc='calc_semblance'):
         for t in tqdm(range(len(vertical_delay_time)), desc='v=' + str(v)):
-            if args.CMP:
-                power_list = []
-                for i in range(int(nrx/2)):
-                    semblance_value = calc_semblance(v, t, i)
-                    power_list.append(np.sum(semblance_value))
-                #semblance_value = sum([sum(calc_semblance(v, t, i)) for i in range(int(nrx/2))])**2
-                #semblance_map[t, v] = semblance_value`
-                semblance_map[t, v] = np.sum(power_list)**2
-            else:
-                semblance_map[t, v] = (np.array([calc_semblance(v, t, i) for i in range(nrx)]).sum())**2
+            power_list = []
+            for i in range(obs_num):
+                semblance_value = calc_semblance(v, t, i)
+                power_list.append(np.sum(semblance_value))
+            #semblance_value = sum([sum(calc_semblance(v, t, i)) for i in range(int(nrx/2))])**2
+            #semblance_map[t, v] = semblance_value`
+            semblance_map[t, v] = np.sum(power_list)**2
+
+    return semblance_map
+"""
+
+
+#* Palarellel processing
+def worker_func(args):
+    v, t, RMS_velocity, vertical_delay_time, obs_num = args
+    power_list = [np.sum(calc_semblance(v, t, i)) for i in range(obs_num)]
+    return t, v, np.sum(power_list)**2
+
+def calc_semblance_parallel(RMS_velocity, vertical_delay_time, obs_num):
+    semblance_map = np.zeros((len(vertical_delay_time), len(RMS_velocity)))
+    args = [(v, t, RMS_velocity, vertical_delay_time, obs_num) \
+            for v in range(len(RMS_velocity)) for t in range(len(vertical_delay_time))]
+
+    with Pool() as pool:
+        results = list(tqdm(pool.imap(worker_func, args), total=len(args), desc='Calculating Semblance'))
+
+    for t, v, semblance_value in results:
+        semblance_map[t, v] = semblance_value
 
     return semblance_map
 
 
 
-#* make output directory
-output_dir_path = data_dir_path + '/Vrms_DePue'
-if not os.path.exists(output_dir_path):
-    os.makedirs(output_dir_path)
 
-
-
-#* load only or calculate and save
-"""
-select area [ns]
-"""
-select_start = 60
-select_end = 100
-select_startx = 0
-select_endx = 1.0
-"""
-select area [ns]
-"""
-
+# Ensure this part is under the __main__ guard
 if __name__ == '__main__':
-    freeze_support()
+
+
+    #* make output directory
+    output_dir_path = os.path.join(os.path.dirname(args.jsonfile), 'Vrms')
+    if not os.path.exists(output_dir_path):
+        os.makedirs(output_dir_path)
+
+
+
+    #* load only or calculate and save
+    """
+    select area [ns]
+    """
+    select_start = 70
+    select_end = 100
+    select_startx = 0
+    select_endx = 1
+    """
+    select area [ns]
+    """
 
     #* plot only
     if args.plot_type == 'plot':
@@ -236,18 +193,16 @@ if __name__ == '__main__':
     #* make select plot
     elif args.plot_type == 'select':
         Vt_map = np.loadtxt(params['semblance_txt'], delimiter=',') # load data
-        Vt_map = Vt_map[int(select_start/params['time_step']): int(select_end/params['time_step']), int(select_startx/0.02): int(select_endx/0.02)] # select area
+        Vt_map = Vt_map[int(select_start/params['time_step']): int(select_end/params['time_step']),
+                        int(select_startx/v_interval): int(select_endx/v_interval)] # select area
         Vt_map = Vt_map / np.amax(Vt_map) # normalize by max value in selected area
 
     #* calculate and save as txt file
     elif args.plot_type == 'calc':
-        Vt_map = roop()
-        print(np.amax(Vt_map))
+        #Vt_map = roop()
+        Vt_map = calc_semblance_parallel(RMS_velocity, vertical_delay_time, obs_num)
         Vt_map = Vt_map / np.amax(Vt_map) # normalize
-        if args.CMP:
-            np.savetxt(output_dir_path + '/semblance_map_CMP.txt', Vt_map, delimiter=',')
-        else:
-            np.savetxt(output_dir_path + '/semblance_map.txt', Vt_map, delimiter=',')
+        np.savetxt(output_dir_path + '/semblance_map.txt', Vt_map, delimiter=',')
     else:
         print('error, input plot, mask, or calc')
 
@@ -256,6 +211,14 @@ if __name__ == '__main__':
     #* plot
     fig = plt.figure(figsize=(15, 15))
     ax = fig.add_subplot(111)
+
+    #* show true value points
+    Vrms_theory = params['Vrms_theory'] # [/c]
+    t0_theory = params['t0_theory'] # [ns]
+    plt.scatter(Vrms_theory, t0_theory, c='k', s=70, marker='P', edgecolors='w', label='Theoretical value')
+
+
+
     if args.plot_type == 'select':
         bounds = np.array([0, 0.25, 0.50, 0.75, 1.0])
         cmap = mpl.colors.ListedColormap([plt.cm.Blues(int(255*i/3)) for i in range(4)])
@@ -270,15 +233,21 @@ if __name__ == '__main__':
     else:
         plt.imshow(Vt_map, cmap='jet', aspect='auto', interpolation='nearest',
                 extent=[0, RMS_velocity[-1], Vt_map.shape[0]*params['time_step'], 0],
-                norm=colors.LogNorm(vmin=1e-10, vmax=0.1) #! default: vmin=1e-7, vmax=0.1
+                norm=colors.LogNorm(vmin=1e-7, vmax=0.1) #! default: vmin=1e-7, vmax=0.1
                 #norm=colors.LogNorm(vmin=1e-7, vmax=0.5)
         )
+
+
 
     ax.set_xlabel('RMS velocity [/c]', fontsize=20)
     ax.set_ylabel('Vertical delay time [ns]', fontsize=20)
     ax.grid(color='gray', linestyle='--', which='both', linewidth=0.5)
     ax.tick_params(labelsize=18)
+    plt.legend(fontsize=18, loc='lower right')
 
+
+
+    #* colorbar
     delvider = axgrid1.make_axes_locatable(ax)
     cax = delvider.append_axes('right', size='5%', pad=0.1)
     plt.colorbar(cax=cax, label='Cross-correlation')
@@ -286,13 +255,11 @@ if __name__ == '__main__':
 
 
     #* save plot
-    if args.plot_type == 'select' and args.closeup == False:
+    if args.plot_type == 'select':
         output_dir_path = output_dir_path + '/select'
         if not os.path.exists(output_dir_path):
             os.makedirs(output_dir_path)
         plt.savefig(output_dir_path + '/semblance_map_select' + str(select_start) + '-' + str(select_end) + '.png')
-    elif args.CMP:
-        plt.savefig(output_dir_path + '/semblance_map_CMP.png')
     else:
         plt.savefig(output_dir_path + '/semblance_map.png')
     plt.show()
