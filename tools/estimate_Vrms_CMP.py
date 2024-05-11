e17e525869c70f61c82efa430363bb2612a19079
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2024-05-11 16:43:58 +0900

    plot確認

--- tools/estimate_Vrms_CMP.py
+++ tools/estimate_Vrms_CMP.py
@@ -165,7 +165,7 @@ else:
     plt.imshow(corr_map,
                 cmap = 'jet', aspect='auto',interpolation='nearest',
                 extent=[Vrms_array_percent[0], Vrms_array_percent[-1], t0_array_ns[-1], t0_array_ns[0]],
-                norm = colors.LogNorm(vmin=1e-5, vmax=1)
+                norm = colors.LogNorm(vmin=1e-10, vmax=1)
                 )
 
 
