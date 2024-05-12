9aa90c53645dc633c005fab1662970fc3480e192
{keitaro-kanda} <{apollo11keitaro@gmail.com}>
2024-05-12 16:11:30 +0900

    select remake

--- tools/calc_Vrms_from_geometry.py
+++ tools/calc_Vrms_from_geometry.py
@@ -19,13 +19,13 @@ class calc_Vrms:
     def load_params_from_json(self):
         with open(self.jsonfile_path) as f:
             params = json.load(f)
-        
+
         self.layer_thickness = np.array(params['layering_structure_info']['layer_thickness']) # [m]
         self.internal_permittivity = np.array(params['layering_structure_info']['internal_permittivity']) # []
         self.internal_velovity = np.array(self.c/ np.sqrt(self.internal_permittivity)) # [m/s]
 
         return self.layer_thickness, self.internal_permittivity, self.internal_velovity
-    
+
     def calc_t0(self, layer_thickness, internal_velovity):
         #* calculate t0 [s]
         self.t0 = []
@@ -35,7 +35,7 @@ class calc_Vrms:
             else:
                 self.t0.append(2 * layer_thickness[i] / internal_velovity[i] + self.t0[i-1])
         return np.array(self.t0)
-    
+
     def calc_Vrms(self, layer_thickness, internal_velocity, t0):
         #* calculate Vrms [/c]
         self.Vrms = []
@@ -45,8 +45,8 @@ class calc_Vrms:
                 Vrms_bunbo.append(2 * layer_thickness[i] * internal_velocity[i])
                 self.Vrms.append(
                     np.sqrt(
-                        (2 * layer_thickness[i] * internal_velocity[i]) / (t0[i])
-                    ))
+                        (2 *layer_thickness[i] * internal_velocity[i]) / (t0[i])
+                    ) / self.c)
             else:
                 Vrms_bunbo.append(2 * layer_thickness[i] * internal_velocity[i])
                 self.Vrms.append(
@@ -60,7 +60,7 @@ class calc_Vrms:
 #* example usage
 """
 jsonfile_path = 'kanda/domain_50x100/no_loss/geometry/geometry.json'
-calc_Vrms = calc_Vrms_from_geometry(jsonfile_path)
+calc_Vrms = calc_Vrms(jsonfile_path)
 layer_thickness, internal_permittivity, internal_velovity = calc_Vrms.load_params_from_json()
 t0 = calc_Vrms.calc_t0(layer_thickness, internal_velovity)
 Vrms = calc_Vrms.calc_Vrms(layer_thickness, internal_velovity, t0)
