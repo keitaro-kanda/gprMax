3dcc5b17bb2c032f4d215df19baa438f886b9937
keitaro kanda MacStudio <{apollo11keitaro@gmail.com}>
2024-07-09 17:01:32 +0900

    雑な例

--- tools/plot_Bscan.py
+++ tools/plot_Bscan.py
@@ -18,6 +18,7 @@
 
 import argparse
 import os
+import json
 
 import h5py
 import matplotlib.pyplot as plt
@@ -53,14 +54,23 @@ def mpl_plot(filename, outputdata, dt, rxnumber, rxcomponent, closeup=False):
                      figsize=(15, 15), facecolor='w', edgecolor='w')
     
 
-    # normalize
+    #* normalize
     outputdata_norm = outputdata / np.amax(np.abs(outputdata)) * 100
-    print(np.amax(outputdata_norm))
+
+    src_step = params['antenna_settings']['src_step']
+    rx_step = params['antenna_settings']['rx_step']
+
+    if src_step == rx_step:
+        antenna_step = src_step
+    elif np.abs(src_step) == np.abs(rx_step):
+        antenna_step = np.abs(src_step) * 2
+    else:
+        antenna_step = 1
 
 
-    plt.imshow(outputdata_norm, 
-             extent=[0, outputdata_norm.shape[1] * 1, outputdata_norm.shape[0] * dt, 0], 
-            interpolation='nearest', aspect='auto', cmap='seismic', 
+    plt.imshow(outputdata_norm,
+             extent=[0, outputdata_norm.shape[1] * antenna_step, outputdata_norm.shape[0] * dt, 0],
+            interpolation='nearest', aspect='auto', cmap='seismic',
             vmin=-1, vmax=1)
     plt.xlabel('Offset distance [m]', fontsize=20)
     plt.ylabel('Time [ns]', fontsize=20)
@@ -69,19 +79,20 @@ def mpl_plot(filename, outputdata, dt, rxnumber, rxcomponent, closeup=False):
     # =====closeupオプション=====
     if closeup:
 
-        closeup_start = 540 # [ns]
-        closeup_end = 590 # [ns]
+        closeup_start = 0 # [ns]
+        closeup_end = 250 # [ns]
+        #closeup_start = int(closeup_start / dt)
+        #closeup_end = int(closeup_end / dt)
         plt.imshow(outputdata_norm,
-             extent=[0, outputdata_norm.shape[1] * 0.5, outputdata_norm.shape[0] * dt, 0],
+             extent=[0, outputdata_norm.shape[1] * antenna_step, outputdata_norm.shape[0] * dt, 0],
             interpolation='nearest', aspect='auto', cmap='seismic',
-            vmin=-0.01, vmax=0.01)
-        #plt.xlim(35, 55)
-        plt.ylim(closeup_end*10**(-9), closeup_start*10**(-9))
+            vmin=-1, vmax=1)
+        plt.ylim(closeup_end, closeup_start)
         plt.minorticks_on( )
 
 
     if closeup:
-        plt.title('rx' + str(rxnumber) + ' closeup: '+str(closeup_start)+'-'+str(closeup_end), fontsize=20)
+        plt.title('rx' + str(rxnumber) + ' closeup: '+str(closeup_start)+'-'+str(closeup_end) + '[ns]', fontsize=20)
     else:
         plt.title('rx' + str(rxnumber), fontsize=20)
 
@@ -117,8 +128,9 @@ if __name__ == "__main__":
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Plots a B-scan image.', 
-                                     usage='cd gprMax; python -m tools.plot_Bscan outputfile output -closeup --select-rx') 
-    parser.add_argument('outputfile', help='name of output file including path')
+                                     usage='cd gprMax; python -m tools.plot_Bscan jsonfile output -closeup --select-rx')
+    #parser.add_argument('outputfile', help='name of output file including path')
+    parser.add_argument('jsonfile', help='name of json file including path')
     parser.add_argument('rx_component', help='name of output component to be plotted', 
                         choices=['Ex', 'Ey', 'Ez', 'Hx', 'Hy', 'Hz', 'Ix', 'Iy', 'Iz'])
     # closeupのオプションを作る
@@ -126,8 +138,14 @@ if __name__ == "__main__":
     parser.add_argument('--select-rx', action='store_true', help='select rx number', default=False)
     args = parser.parse_args()
 
-    # Open output file and read number of outputs (receivers)
-    f = h5py.File(args.outputfile, 'r') # outファイルの読み込み？
+    #* Load json file
+    with open (args.jsonfile) as f:
+        params = json.load(f)
+    outfile_path = params['out_file']
+
+    #* Open output file and read number of outputs (receivers)
+    #f = h5py.File(args.outputfile, 'r') # outファイルの読み込み？
+    f = h5py.File(outfile_path, 'r')
     nrx = f.attrs['nrx'] # Attribute(属性)の読み取り？、nrx:レシーバーの総数
     print('nrx: ', nrx)
     f.close()
@@ -139,12 +157,12 @@ if __name__ == "__main__":
     # データの取得とプロットの作成を実行？
     if args.select_rx:
         rx = 1
-        outputdata, dt = get_output_data(args.outputfile, rx, args.rx_component)
+        outputdata, dt = get_output_data(outfile_path, rx, args.rx_component)
         dt = dt * 1e9 # Convert to [ns]
-        plthandle = mpl_plot(args.outputfile, outputdata, dt, rx, args.rx_component, args.closeup)
+        plthandle = mpl_plot(outfile_path, outputdata, dt, rx, args.rx_component, args.closeup)
     else:
         for rx in range(1, nrx + 1):
-            outputdata, dt = get_output_data(args.outputfile, rx, args.rx_component)
+            outputdata, dt = get_output_data(outfile_path, rx, args.rx_component)
             dt = dt * 1e9 # Convert to [ns]
-            plthandle = mpl_plot(args.outputfile, outputdata, dt, rx, args.rx_component, args.closeup)
+            plthandle = mpl_plot(outfile_path, outputdata, dt, rx, args.rx_component, args.closeup)
     plthandle.show()
