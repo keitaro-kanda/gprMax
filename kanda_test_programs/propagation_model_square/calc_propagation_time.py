a0db8f7c2920dba2a2617ea28df69926b7c8f2a6
keitaro kanda MacStudio <{apollo11keitaro@gmail.com}>
2024-12-04 21:48:21 +0900

    最小時間差のプロットを追加

--- kanda_test_programs/propagation_model_square/calc_propagation_time.py
+++ kanda_test_programs/propagation_model_square/calc_propagation_time.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import mpl_toolkits.axes_grid1 as axgrid1
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.gridspec import GridSpec
+from tqdm import tqdm
 
 # Physical constants
 c0 = 299792458  # Speed of light in vacuum [m/s]
@@ -36,7 +37,7 @@ print('Tb:', Tb.shape)
 #* Side component
 Ls = np.zeros((len(theta_array), len(rock_size_array)))  # [m]
 print('Ls:', Ls.shape)
-for i, theta in enumerate(theta_array):
+for i, theta in tqdm(enumerate(theta_array), desc='Calculating side component...'):
     for j, rock_size in enumerate(rock_size_array):
         side_criteria_1 = antenna_height * np.tan(theta) + rock_depth * (np.sin(theta)) / (np.sqrt(3 - np.sin(theta)**2))  # [m]
         side_criteria_2 = antenna_height * np.tan(theta) + rock_depth * (np.sin(theta)) / (np.sqrt(9 - np.sin(theta)**2))\
@@ -53,101 +54,95 @@ Ts = Ls / c0  # [s]
 print('Ts:', Ts.shape)
 
 
+#* Calculate the time difference
 delta_T = Ts - Tb  # [s]
 
 
+#* Survey the smallest time difference in each rock size
+min_delta_T = np.zeros(len(rock_size_array))
+# 'nan'の場合は、最小値を取らないようにする
+for i in range(len(rock_size_array)):
+    if Ts[:, i].all() == 'nan':
+        min_delta_T[i] = 'nan'
+    else:
+        min_delta_T[i] = np.nanmin(delta_T[:, i])
 
-#* Plot
-fig, ax = plt.subplots(1, 3, figsize=(20, 7), tight_layout=True)
+print('min_delta_T:', min_delta_T.shape)
+print('min_delta_T:', min_delta_T)
 
 
-#* Plot colormap
-color = 'jet'
-colorbar_size = '10%'
-colorbar_pad = 1
+
+#* Create a GridSpec layout
+fig = plt.figure(figsize=(20, 7))
+gs = GridSpec(1, 3, figure=fig, width_ratios=[1, 1, 1])  # Equal-sized panels
 
 #* Bottom component
-ax[0].plot(rock_size_array, Tb[0] / 1e-9, label='Bottom', linewidth=2)
-ax[0].set_title('Bottom component', fontsize=24)
-
-ax[0].set_ylabel('Time [ns]', fontsize=20)
-
+ax0 = fig.add_subplot(gs[0, 0])
+ax0.plot(rock_size_array, Tb[0] / 1e-9, label='Bottom', linewidth=2)
+ax0.set_title('Bottom component', fontsize=24)
+ax0.set_ylabel('Two-way travel ime [ns]', fontsize=20)
+ax0.set_aspect('auto')
 
 #* Side component
-
-im1 = ax[1].imshow(Ts / 1e-9,
-            extent=(rock_size_array[0], rock_size_array[-1], theta_array[0], theta_array[-1]),
-            origin='lower', # theta=0が下に来るように
-            cmap = color,
-            #min=25, vmax=75
-            )
-ax[1].set_title('Side component', fontsize=24)
-
-delvider1 = axgrid1.make_axes_locatable(ax[1])
-cax1 = delvider1.append_axes('bottom', size=colorbar_size, pad=colorbar_pad)
+ax1 = fig.add_subplot(gs[0, 1])
+im1 = ax1.imshow(
+    Ts / 1e-9,
+    extent=(rock_size_array[0], rock_size_array[-1], theta_array[0], theta_array[-1]),
+    origin='lower',
+    cmap='jet',
+)
+ax1.set_title('Side component', fontsize=24)
+divider1 = make_axes_locatable(ax1)
+cax1 = divider1.append_axes('bottom', size='10%', pad=1)
 cbar1 = plt.colorbar(im1, cax=cax1, orientation='horizontal')
-cbar1.set_label('Time [ns]', fontsize=20)
+cbar1.set_label('Two-way travel time [ns]', fontsize=20)
 cbar1.ax.tick_params(labelsize=18)
 
 #* Time difference
-im2 = ax[2].imshow(delta_T / 1e-9,
-            extent=(rock_size_array[0], rock_size_array[-1], theta_array[0], theta_array[-1]),
-            origin='lower', # theta=0が下に来るように
-            cmap = color,
-            #vmin=0, vmax=25
-            )
-ax[2].set_title('Time difference', fontsize=24)
-
-delvider2 = axgrid1.make_axes_locatable(ax[2])
-cax2 = delvider2.append_axes('bottom', size=colorbar_size, pad=colorbar_pad)
+ax2 = fig.add_subplot(gs[0, 2])
+im2 = ax2.imshow(
+    delta_T / 1e-9,
+    extent=(rock_size_array[0], rock_size_array[-1], theta_array[0], theta_array[-1]),
+    origin='lower',
+    cmap='jet',
+)
+ax2.set_title('Time difference of two components', fontsize=24)
+divider2 = make_axes_locatable(ax2)
+cax2 = divider2.append_axes('bottom', size='10%', pad=1)
 cbar2 = plt.colorbar(im2, cax=cax2, orientation='horizontal')
-cbar2.set_label('Time [ns]', fontsize=20)
+cbar2.set_label('Time difference [ns]', fontsize=20)
 cbar2.ax.tick_params(labelsize=18)
-
 
 #* y軸のメモリをpiに変換
 y_ticks = np.arange(0, np.pi * 5 / 8, np.pi / 8)
 y_labels = ['0', r'$\frac{\pi}{8}$', r'$\frac{\pi}{4}$', r'$\frac{3\pi}{8}$', r'$\frac{\pi}{2}$']
-for i in range(3):
-    ax[i].set_xlabel('Rock size [m]', fontsize=20)
+for ax in [ax1, ax2]:
+    ax.set_yticks(y_ticks)
+    ax.set_yticklabels(y_labels)
+    ax.set_ylabel('Initial transmission angle', fontsize=20)
+    ax.set_ylim(0, np.pi / 4)
+    ax.set_xlim(1.0, np.max(rock_size_array))
 
-    ax[i].tick_params(axis='both', labelsize=18)
+#* Add common elements to all axes
+for ax in [ax0, ax1, ax2]:
+    ax.set_xlabel('Rock size [m]', fontsize=20)
+    ax.tick_params(axis='both', labelsize=18)
+    ax.grid(axis='both')
 
-    ax[i].grid(axis='both')
-
-    if i != 0:
-        ax[i].set_yticks(y_ticks)
-        ax[i].set_yticklabels(y_labels)
-        ax[i].set_ylabel('Initial transmission angle', fontsize=20)
-        ax[i].set_ylim(0, np.pi /4)
-
-
+plt.tight_layout()
 plt.savefig('kanda_test_programs/propagation_model_square/propagation_time.png')
 plt.show()
 
 
-"""
-plt.figure(figsize=(12, 10), tight_layout=True)
 
-# 第1縦軸，Tb, Ts vs rock_sizeのプロット
-plt.plot(rock_size, Tb / 1e-9, label='Bottom', linewidth=2, color='red')
-plt.plot(rock_size, Ts / 1e-9, label='Side', linewidth=2, color='blue')
+#* Plot the minimum time difference
+plt.figure(figsize=(10, 8))
+plt.plot(rock_size_array, min_delta_T / 1e-9, linewidth=2)
 
-plt.xlabel('Rock size [m]', fontsize=24)
-plt.ylabel('Time [ns]', fontsize=24)
-plt.tick_params(labelsize=20)
-plt.legend(fontsize=20)
-plt.grid(axis='both')
+plt.xlabel('Rock size [m]', fontsize=20)
+plt.ylabel('Minimum time difference [ns]', fontsize=20)
+plt.tick_params(labelsize=18)
+plt.grid()
 
-# 第2縦軸，delta_T vs rock_sizeのプロット
-plt.twinx()
-plt.plot(rock_size, delta_T / 1e-9, color='green')
-
-
-plt.ylabel('Time difference [ns]', color='green', fontsize=24)
-plt.tick_params(axis='y', labelcolor='green', labelsize=20)
-plt.ylim(2.5, 4.5)
-
-
+plt.savefig('kanda_test_programs/propagation_model_square/min_time_difference.png')
 plt.show()
-"""
