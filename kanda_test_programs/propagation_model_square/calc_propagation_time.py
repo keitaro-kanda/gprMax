453e8d627be9be5a5ccc1fd913c0e6e77eec0eab
keitaro kanda MacStudio <{apollo11keitaro@gmail.com}>
2024-12-10 17:21:04 +0900

    振るパラメータを変更

--- kanda_test_programs/propagation_model_square/calc_propagation_time.py
+++ kanda_test_programs/propagation_model_square/calc_propagation_time.py
@@ -21,55 +21,54 @@ h_dash = antenna_height
 
 
 #* Parameters
-rock_heights = np.arange(0, 2.101, 0.001)  # [m]
-rock_widths = np.arange(0.3, 2.11, 0.3)  # [m]
+#rock_heights = np.arange(0, 2.101, 0.001)  # [m]
+#rock_widths = np.arange(0.3, 2.11, 0.3)  # [m]
+rock_heights = np.arange(0.30, 2.11, 0.30)  # [m]
+rock_widths = np.arange(0, 6.01, 0.01)  # [m]
 thetas = np.arange(0, np.pi * 1/2, np.pi / 1440)  # [rad], 0 ~ pi/2
 
 
 
 #* Bottom component
 Lb = 2 * (antenna_height + rock_depth * np.sqrt(er_regolith) + rock_heights * np.sqrt(er_rock))  # [m]
-Tb = Lb / c0  # [s]
-
-Tb = np.vstack([Tb for _ in range(len(thetas))])
-print('Tb:', Tb.shape)
+Tb = Lb / c0  # [s], number of component is the same as the number of rock_heights
 
 
 
 #* Define function to calculate the side component
-def calc_side_component(rock_width):
-    Ls = np.zeros((len(thetas), len(rock_heights)))  # [m]
-    #print('Ls:', Ls.shape)
-    for i, theta in tenumerate(thetas, desc=f'Rock width: {rock_width:.1f} m'):
-        for j, h in enumerate(rock_heights):
+def calc_side_component(height_index, Tb_at_height):
+    Ls = np.zeros((len(thetas), len(rock_widths)))  # [m]
+
+    height = rock_heights[height_index]
+    for i, theta in tenumerate(thetas, desc=f'Rock height: {height:.1f} m'):
+        for j, width in enumerate(rock_widths):
             # Criteriaの計算
             side_criteria_1 = antenna_height * np.tan(theta) + rock_depth * (np.sin(theta)) / (np.sqrt(3 - np.sin(theta)**2))  # [m]
-            side_criteria_2 = side_criteria_1 + h * (np.sin(theta)) / (np.sqrt(9 - np.sin(theta)**2))  # [m]
+            side_criteria_2 = side_criteria_1 + height * (np.sin(theta)) / (np.sqrt(9 - np.sin(theta)**2))  # [m]
 
             # 条件を判定
-            if side_criteria_1 < rock_width / 2 < side_criteria_2:
+            if side_criteria_1 < width / 2 < side_criteria_2:
                 h_dash = (2 * np.cos(theta)**2 - 1) * antenna_height + \
-                            (rock_width - 2 * np.sin(theta) * (rock_depth / np.sqrt(3 - np.sin(theta)**2) + h / np.sqrt(9 - np.sin(theta)**2))) * \
+                            (width - 2 * np.sin(theta) * (rock_depth / np.sqrt(3 - np.sin(theta)**2) + height / np.sqrt(9 - np.sin(theta)**2))) * \
                             np.sin(theta) * np.cos(theta) # [m]
                 Ls[i, j] = (antenna_height + h_dash) / np.cos(theta) \
                     + (6 * rock_depth) / (np.sqrt(3 - np.sin(theta)**2)) \
-                    + (18 * h) / (np.sqrt(9 - np.sin(theta)**2))  # [m]
+                    + (18 * height) / (np.sqrt(9 - np.sin(theta)**2))  # [m]
             else:
                 Ls[i, j] = 'nan'
 
     Ts = Ls / c0  # [s]
-    #print('Ts:', Ts.shape)
 
     #* Calculate the time difference
-    delta_T = Ts - Tb  # [s]
+    delta_T = Ts - Tb_at_height  # [s]
 
     return Ts, delta_T
 
 
 #* Define function to calculate minimum time difference
 def calc_min_time_difference(delta_T):
-    min_delta_T = np.zeros(len(rock_heights))  # 結果を格納する配列
-    for i in range(len(rock_heights)):
+    min_delta_T = np.zeros(len(rock_widths))  # 結果を格納する配列
+    for i in range(len(rock_widths)):
         # delta_T[:, i] に NaN 以外の値があるかチェック
         if np.all(np.isnan(delta_T[:, i])):
             min_delta_T[i] = np.nan  # 全てがNaNの場合は NaN を設定
@@ -79,14 +78,17 @@ def calc_min_time_difference(delta_T):
 
 
 
-def plot(Ts, delta_T, min_delta_T):
+def plot(height, Ts, delta_T, min_delta_T):
     #* Create a GridSpec layout
     fig = plt.figure(figsize=(20, 8))
     gs = GridSpec(1, 3, figure=fig, width_ratios=[1, 1, 1])  # Equal-sized panels
 
     #* Bottom component
     ax0 = fig.add_subplot(gs[0, 0])
-    ax0.plot(rock_heights, Tb[0] / 1e-9, linewidth=2)
+    ax0.plot(rock_heights, Tb / 1e-9, linewidth=2)
+    ax0.axvline(height, color='red', linestyle='--', linewidth=3)
+
+    ax0.set_xlabel('Rock height [m]', fontsize=20)
     ax0.set_title('Bottom component', fontsize=24)
     ax0.set_ylabel('Two-way travel time [ns]', fontsize=20)
 
@@ -94,7 +96,7 @@ def plot(Ts, delta_T, min_delta_T):
     ax1 = fig.add_subplot(gs[0, 1])
     im1 = ax1.imshow(
         Ts / 1e-9,
-        extent=(rock_heights[0], rock_heights[-1], thetas[0], thetas[-1]),
+        extent=(rock_widths[0], rock_widths[-1], thetas[0], thetas[-1]),
         origin='lower',
         cmap='jet',
     )
@@ -109,7 +111,7 @@ def plot(Ts, delta_T, min_delta_T):
     ax2 = fig.add_subplot(gs[0, 2])
     im2 = ax2.imshow(
         delta_T / 1e-9,
-        extent=(rock_heights[0], rock_heights[-1], thetas[0], thetas[-1]),
+        extent=(rock_widths[0], rock_widths[-1], thetas[0], thetas[-1]),
         origin='lower',
         cmap='jet',
     )
@@ -124,22 +126,22 @@ def plot(Ts, delta_T, min_delta_T):
     y_ticks = np.arange(0, np.pi * 5 / 8, np.pi / 8)
     y_labels = ['0', r'$\frac{\pi}{8}$', r'$\frac{\pi}{4}$', r'$\frac{3\pi}{8}$', r'$\frac{\pi}{2}$']
     for ax in [ax1, ax2]:
+        ax.set_xlabel('Rock width [m]', fontsize=20)
         ax.set_yticks(y_ticks)
         ax.set_yticklabels(y_labels)
         ax.set_ylabel('Initial transmission angle', fontsize=20)
-        ax.set_ylim(0, np.pi / 4)
+        #ax.set_ylim(0, np.pi / 4)
         #ax.set_xlim(1.0, np.max(rock_heights))
 
     #* Add common elements to all axes
     for ax in [ax0, ax1, ax2]:
-        ax.set_xlabel('Rock height [m]', fontsize=20)
         ax.tick_params(axis='both', labelsize=18)
         ax.grid(axis='both')
         ax.set_aspect('auto')
 
-    fig.suptitle(f'Rock width: {w:.1f} m', fontsize=24)
+    fig.suptitle(f'Rock height: {height:.1f} m', fontsize=24)
     plt.tight_layout()
-    plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/propagation_time_w{w:.1f}.png')
+    plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/propagation_time_h{height:.1f}.png')
     #plt.show()
     plt.close()
 
@@ -147,22 +149,23 @@ def plot(Ts, delta_T, min_delta_T):
 
     #* Plot the minimum time difference
     plt.figure(figsize=(8, 6), tight_layout=True)
-    plt.plot(rock_heights, min_delta_T / 1e-9, linewidth=2)
+    plt.plot(rock_widths, min_delta_T / 1e-9, linewidth=2)
 
-    plt.title(f'Rock width: {w:.1f} m', fontsize=24)
-    plt.xlabel('Rock height [m]', fontsize=20)
+    plt.title(f'Rock height: {height:.1f} m', fontsize=24)
+    plt.xlabel('Rock width [m]', fontsize=20)
     plt.ylabel('Minimum time difference [ns]', fontsize=20)
     plt.tick_params(labelsize=18)
     plt.grid()
 
-    plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/min_time_difference_w{w:.1f}.png')
+    plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/min_time_difference_h{height:.1f}.png')
     plt.close()
 
 
 
 #* main
 if __name__ == '__main__':
-    for w in tqdm(rock_widths):
-        Ts, delta_T = calc_side_component(w)
+    for i, h in tenumerate(rock_heights, desc='Rock height'):
+        Tb_i = np.tile(Tb[i], (len(thetas), len(rock_widths)))  # [s]
+        Ts, delta_T = calc_side_component(i, Tb_i)
         min_delta_T = calc_min_time_difference(delta_T)
-        plot(Ts, delta_T, min_delta_T)
+        plot(h, Ts, delta_T, min_delta_T)
