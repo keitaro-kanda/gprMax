d6ba5232241f648c3b411a529f2a307ea02527e6
keitaro kanda MacBookAir <{apollo11keitaro@gmail.com}>
2024-12-15 15:55:04 +0900

    フォルダ変更 FDTDとの比較を実装

--- kanda_test_programs/propagation_path_model.py
+++ kanda_test_programs/propagation_path_model.py
@@ -68,13 +68,15 @@ def calc_side_component(height_index, Tb_at_height):
 #* Define function to calculate minimum time difference
 def calc_min_time_difference(delta_T):
     min_delta_T = np.zeros(len(rock_widths))  # 結果を格納する配列
+    mean_delta_T = np.zeros(len(rock_widths))  # 結果を格納する配列
     for i in range(len(rock_widths)):
         # delta_T[:, i] に NaN 以外の値があるかチェック
         if np.all(np.isnan(delta_T[:, i])):
             min_delta_T[i] = np.nan  # 全てがNaNの場合は NaN を設定
         else:
             min_delta_T[i] = np.nanmin(delta_T[:, i])  # NaN以外の最小値を取得
-    return min_delta_T
+            mean_delta_T[i] = np.nanmean(delta_T[:, i])  # NaN以外の平均値を取得
+    return min_delta_T, mean_delta_T
 
 
 #* Define function to calculate mean and standard deviation of time difference
@@ -93,7 +95,7 @@ def calc_time_difference(delta_T):
 
 
 
-def plot(height, Ts, delta_T, min_delta_T, delta_T_mean, delta_T_std):
+def plot(height, Ts, delta_T, min_delta_T, mean_delta_T):
     #* Create a GridSpec layout
     fig = plt.figure(figsize=(20, 8))
     gs = GridSpec(1, 3, figure=fig, width_ratios=[1, 1, 1])  # Equal-sized panels
@@ -164,18 +166,20 @@ def plot(height, Ts, delta_T, min_delta_T, delta_T_mean, delta_T_std):
 
     #* Plot the minimum time difference
     plt.figure(figsize=(8, 6), tight_layout=True)
-    plt.plot(rock_widths, min_delta_T / 1e-9, linewidth=2)
+    plt.plot(rock_widths, min_delta_T / 1e-9, linewidth=2, label='Min')
+    #plt.plot(rock_widths, mean_delta_T / 1e-9, linewidth=2, label='Mean', linestyle='-.')
 
     plt.title(f'Rock height: {height:.1f} m', fontsize=24)
     plt.xlabel('Rock width [m]', fontsize=20)
     plt.ylabel('Minimum time difference [ns]', fontsize=20)
     plt.tick_params(labelsize=18)
     plt.grid()
+    plt.legend(fontsize=18)
 
     plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/min_time_difference_h{height:.1f}.png')
     plt.close()
 
-
+    """
     #* Plot the mean and standard deviation of time difference
     plt.figure(figsize=(8, 6), tight_layout=True)
     plt.errorbar(rock_widths, delta_T_mean / 1e-9, yerr=delta_T_std / 1e-9, fmt='o', markersize=5, color='gray', alpha=0.5)
@@ -190,6 +194,33 @@ def plot(height, Ts, delta_T, min_delta_T, delta_T_mean, delta_T_std):
     plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/mean_std_time_difference_h{height:.1f}.png')
     #plt.show()
     plt.close()
+    """
+
+
+def compare_with_FDTD(height, min_delta_T):
+    #* 現状計算している高さ30 cm, 60 cmの場合のFDTD結果のみ実装
+    if height == 0.3:
+        time_difference_FDTD = [[1.8, 1.82], [2.4, 3.04], [2.7, 3.74], [3.0, 4.53]]
+    elif height == 0.6:
+        time_difference_FDTD = [[1.8, 1.66], [2.4, 2.83], [3.0, 4.26], [3.6, 5.91], [4.2, 7.71], [4.8, 9.62],
+                                [5.4, 11.59], [6.0, 13.55]]
+    #* Plot the minimum time difference
+    plt.figure(figsize=(8, 6), tight_layout=True)
+    plt.plot(rock_widths, min_delta_T / 1e-9, linewidth=2, label='Model')
+
+    #* Plt the FDTD results
+    for i in range(len(time_difference_FDTD)):
+        plt.plot(time_difference_FDTD[i][0], time_difference_FDTD[i][1], 'o', markersize=5, color='r', label='FDTD' if i == 0 else None)
+
+    plt.title(f'Rock height: {height:.1f} m', fontsize=24)
+    plt.xlabel('Rock width [m]', fontsize=20)
+    plt.ylabel('Minimum time difference [ns]', fontsize=20)
+    plt.tick_params(labelsize=18)
+    plt.grid()
+    plt.legend(fontsize=18)
+
+    plt.savefig(f'/Volumes/SSD_Kanda_BUFFALO/gprMax/propagation_path_model/min_time_difference_h{height:.1f}_compare.png')
+    plt.close()
 
 
 
@@ -199,6 +230,8 @@ if __name__ == '__main__':
     for i, h in tenumerate(rock_heights, desc='Rock height'):
         Tb_i = np.tile(Tb[i], (len(thetas), len(rock_widths)))  # [s]
         Ts, delta_T = calc_side_component(i, Tb_i)
-        min_delta_T = calc_min_time_difference(delta_T)
+        min_delta_T, mean_delta_T = calc_min_time_difference(delta_T)
         delta_T_mean, delta_T_std = calc_time_difference(delta_T)
-        plot(h, Ts, delta_T, min_delta_T, delta_T_mean, delta_T_std)
+        plot(h, Ts, delta_T, min_delta_T, mean_delta_T)
+        if h == 0.3 or h == 0.6:
+            compare_with_FDTD(h, min_delta_T)
