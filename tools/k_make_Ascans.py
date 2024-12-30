04c14dd8990c97e6c0c046969093d51c00f80044
keitaro kanda MacBookAir <{apollo11keitaro@gmail.com}>
2024-12-30 17:17:56 +0900

    task closeup範囲変更

--- tools/k_make_Ascans.py
+++ tools/k_make_Ascans.py
@@ -66,8 +66,8 @@ if __name__ == "__main__":
     # for closeup option
     closeup_x_start = 0 #[ns]
     closeup_x_end =100 #[ns]
-    closeup_y_start = -60
-    closeup_y_end = 60
+    closeup_y_start = -25
+    closeup_y_end = 25
 
     #* Load the transmmit signal data for subtraction
     transmmit_signal_path = '/Volumes/SSD_Kanda_BUFFALO/gprMax/domain_10x6/20241111_polarity_v2/direct/A-scan/direct.out' # 送信波形データを読み込む
