ffe7f4a3b40c77cd2fa787c1f7853c56b48bd8a6
keitaro kanda MacStudio <{apollo11keitaro@gmail.com}>
2025-09-30 17:44:54 +0900

    fix: プロットのデータがない場合のエラーメッセージを追加し、コードを簡素化

--- tools/specialized/polarity_study/plot_size_error.py
+++ tools/specialized/polarity_study/plot_size_error.py
@@ -68,11 +68,20 @@ y = x = np.linspace(6, 15, 100)
 ### plot
 fig, ax = plt.subplots(1, 2, figsize=(14, 6), tight_layout=True)
 
+size_estimations = [LPR_circle, LPR_square, Bipolar_circle, Bipolar_square]
+names = ['LPR_circle', 'LPR_square', 'Bipolar_circle', 'Bipolar_square']
+colors = ['r', 'r', 'b', 'b']
+linestyles = ['-', '--', '-', '--']
 # true size VS estimated size
-ax[0].plot(LPR_circle[:, 0], LPR_circle[:, 1], label='Bipolar-circle', color='r', linestyle='-', marker='o')
-ax[0].plot(LPR_square[:, 0], LPR_square[:, 1], label='Bipolar-square', color='r', linestyle='--', marker='o')
-ax[0].plot(Bipolar_circle[:, 0], Bipolar_circle[:, 1], label='Unipolar-circle', color='b', linestyle='-', marker='o')
-ax[0].plot(Bipolar_square[:, 0], Bipolar_square[:, 1], label='Unipolar-square', color='b', linestyle='--', marker='o')
+for i, data in enumerate(size_estimations):
+    if data.shape[0] == 0:
+        print('No data for one of the configurations. Please check the input txt file.')
+    else:
+        ax[0].plot(data[:, 0], data[:, 1], label=names[i], marker='o')
+# ax[0].plot(LPR_circle[:, 0], LPR_circle[:, 1], label='Bipolar-circle', color='r', linestyle='-', marker='o')
+# ax[0].plot(LPR_square[:, 0], LPR_square[:, 1], label='Bipolar-square', color='r', linestyle='--', marker='o')
+# ax[0].plot(Bipolar_circle[:, 0], Bipolar_circle[:, 1], label='Unipolar-circle', color='b', linestyle='-', marker='o')
+# ax[0].plot(Bipolar_square[:, 0], Bipolar_square[:, 1], label='Unipolar-square', color='b', linestyle='--', marker='o')
 ax[0].plot(x, y, label='y = x', color='k', linestyle='-')
 
 ax[0].set_xlabel('True size [cm]', fontsize=20)
@@ -82,10 +91,15 @@ ax[0].tick_params(labelsize=16)
 ax[0].grid()
 
 # True size VS error
-ax[1].plot(LPR_circle[:, 0], np.abs(LPR_circle[:, 2]), label='Bipolar-circle', color='r', linestyle='-', marker='o')
-ax[1].plot(LPR_square[:, 0], np.abs(LPR_square[:, 2]), label='Bipolar-square', color='r', linestyle='--', marker='o')
-ax[1].plot(Bipolar_circle[:, 0], np.abs(Bipolar_circle[:, 2]), label='Unipolar-circle', color='b', linestyle='-', marker='o')
-ax[1].plot(Bipolar_square[:, 0], np.abs(Bipolar_square[:, 2]), label='Unipolar-square', color='b', linestyle='--', marker='o')
+for i, data in enumerate(size_estimations):
+    if data.shape[0] == 0:
+        print('No data for one of the configurations. Please check the input txt file.')
+    else:
+        ax[1].plot(data[:, 0], np.abs(data[:, 2]), label=names[i], marker='o')
+# ax[1].plot(LPR_circle[:, 0], np.abs(LPR_circle[:, 2]), label='Bipolar-circle', color='r', linestyle='-', marker='o')
+# ax[1].plot(LPR_square[:, 0], np.abs(LPR_square[:, 2]), label='Bipolar-square', color='r', linestyle='--', marker='o')
+# ax[1].plot(Bipolar_circle[:, 0], np.abs(Bipolar_circle[:, 2]), label='Unipolar-circle', color='b', linestyle='-', marker='o')
+# ax[1].plot(Bipolar_square[:, 0], np.abs(Bipolar_square[:, 2]), label='Unipolar-square', color='b', linestyle='--', marker='o')
 
 ax[1].set_xlabel('True size [cm]', fontsize=20)
 ax[1].set_ylabel('Error [%]', fontsize=20)
