822f84fae56212512e1b42c8436332e98555788f
keitaro kanda MacBookAir <{apollo11keitaro@gmail.com}>
2026-06-04 10:38:50 +0900

    outputfiles_mergeのインポートパスを修正し、ファイル名の入力をユーザーから取得するように変更

--- tools/data_processing/k_moving_average.py
+++ tools/data_processing/k_moving_average.py
@@ -4,9 +4,9 @@ import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 
-from tools.outputfiles_merge import get_output_data
+from tools.core.outputfiles_merge import get_output_data
 
-file_name = 'kanda/domain_10x10/rock/B-scan/0.2step_n40/10x10_rock_merged.out'
+file_name = input("Enter the path to the .out file: ").strip()
 
 # .outファイルの読み込み
 output_data = h5py.File(file_name, 'r')
